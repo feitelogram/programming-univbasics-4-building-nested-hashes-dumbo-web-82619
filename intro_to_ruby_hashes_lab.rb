d9b680010708f306804b6a827daab6adbc328502
Nicholas Feitel <nicholas.feitel@gmail.com>
2019-08-08 13:54:58 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -50,7 +50,7 @@ def monopoly_with_fourth_tier
     },
     names: {
       reading_railroad: {"mortgage_value" => "$100"},
-      shortline_railroad: {"mortgage_"},
+      shortline_railroad: {"mortgage_value" => "$800"},
       pennsylvania_railroad: {},
       b_and_o_railroad: {}
       }
